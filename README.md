5dd151d20099757b86c9a3689dfa91e9fd40b62d
david liu <david.liu@autodesk.com>
2014-08-17 23:34:29 -0700

    start upload scripts

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ PBL's own photo manager
 # Overview
 
 # Getting Started
+
+# Motivation/Benefits of Reorganization
+*remove facebook dependency
+* facebook is autocreated from our filesystem
+* back up in two places: imgur and facebook
+* main data store in s3
+* never change
+	* facebook api and imgur api probably change errday (jk but often enough for concern)
+* why imgur?
+	* its an intermediate layer between filesystem and facebook
+	* indirection
+	* one example where this is good: chairs can scrub through images and final-filter. 
+		* perhaps peer filtering
+		* __idea:__ _ALL_ images stored on imgur and a subset of the better ones on facebook
--- imgur_upload.py
+++ imgur_upload.py
@@ -1,3 +1,14 @@
+import os
+import yaml
+
+#
+# load variables from config file
+#
+data = yaml.load(open('config.yaml', 'r'))
+LOCAL_PHOTO_PATH = data['local_photo_path']
+IMGUR_CLIENT_ID = data['imgur_client_id']
+IMGUR_CLIENT_SECRET = data['imgur_client_secret']
+
 
 """
 UPLOAD images from local filesystem onto PBL's imgur account
@@ -7,3 +18,18 @@ TODO
 """
 def upload_images(image_dir):
 	print 'uploading images to imgur...'
+	print '\tclient id '+IMGUR_CLIENT_ID
+	print '\tclient secret '+IMGUR_CLIENT_SECRET
+
+	# get all files on local filesystem
+
+	# create albums in imgur based on directory structure
+
+	# upload images. change local filesystem for restart? (transfer/transferred)
+
+
+
+if __name__ == '__main__':
+	print 'syncing local filesystem with PBL photo archives...'
+	upload_images(LOCAL_PHOTO_PATH)
+
